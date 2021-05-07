857b91036a54303e9aba35051b369928ffb0f4d4
Andrés Cermeño <aecermeno@gmail.com>
2021-05-07 12:41:55 -0400

    Update Statistic.kt for use correct of data

--- app/src/main/java/com/example/carsalesprueba/model/Statistic.kt
+++ app/src/main/java/com/example/carsalesprueba/model/Statistic.kt
@@ -1,20 +1,14 @@
 package com.example.carsalesprueba.model
 
 import android.os.Parcelable
-import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class Statistic(
-    val recovered: Int,
     val deaths: Int,
     val confirmed: Int,
-    val lastChecked: String,
-    val lastReported: String,
-    val location: String
 ) : Parcelable
 
-data class listStatistics(
-    @SerializedName("data")
-    val data: List<Statistic>
+data class AllData(
+    val data: Statistic
 )
