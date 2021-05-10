5fd15a56abff93ba73b58bd96c074aaa25b90670
Andrés Cermeño <aecermeno@gmail.com>
2021-05-10 14:10:20 -0400

    A file is missing

--- app/src/main/java/com/example/carsalesprueba/domain/MyInterceptor.kt
+++ app/src/main/java/com/example/carsalesprueba/domain/MyInterceptor.kt
@@ -7,7 +7,6 @@ class MyInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
             .newBuilder()
-            .get()
             .addHeader("x-rapidapi-key", "96afa298cbmsh913f910f914494cp110c39jsn01a32d68445e")
             .build()
         return chain.proceed(request)
